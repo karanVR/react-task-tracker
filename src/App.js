373033118bb1e1c8c959bd4bb3b83048962b9daf
karanVR <karanveerraghuvanshi@gmail.com>
2021-11-02 20:31:23 +0530

    Task-tracker
    Task-tracker-react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "cric",
+      day: "sat",
+      reminder: "true",
+    },
+    {
+      id: 2,
+      text: "football",
+      day: "sun",
+      reminder: "true",
+    },
+  ]);
+
+  const [showAddTask, setShowAddTask] = useState(false);
+
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10) + 1;
+    const newTask = { id, ...task };  
+    //Using spread Operator concept
+    setTasks([...tasks, newTask]);
+  };
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header
+        title="Tasks"
+        onAdd={() => setShowAddTask(!showAddTask)}
+        showAdd={showAddTask}
+      />
+      {showAddTask && <AddTask onAdd={addTask} />}
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "No Tasks to show!"
+      )}
     </div>
   );
 }
